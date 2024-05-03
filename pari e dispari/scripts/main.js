d18bebbb3477a1f41d7bc5c17e052e606f8ae5a0
samian2002 <samiantariq14@gmail.com>
2024-05-03 04:17:13 +0200

    m2 interazioni utente

---
+++ pari e dispari/scripts/main.js
@@ -0,0 +1,6 @@
+// Chied0 all'utente di scegliere "pari" o "dispari"
+let sceltaUtente = prompt("Scegli 'pari' o 'dispari':");
+
+// Chiedo all'utente di inserire un numero tra 1 e 5
+let numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e 5:"));
+
