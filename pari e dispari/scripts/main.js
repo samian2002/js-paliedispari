a22b21bcd9eb211ba3f9bc660084baad6e4b96e9
samian2002 <samiantariq14@gmail.com>
2024-05-03 04:18:41 +0200

    m2 calcolo computer

--- pari e dispari/scripts/main.js
+++ pari e dispari/scripts/main.js
@@ -4,3 +4,12 @@ let sceltaUtente = prompt("Scegli 'pari' o 'dispari':");
 // Chiedo all'utente di inserire un numero tra 1 e 5
 let numeroUtente = parseInt(prompt("Inserisci un numero tra 1 e 5:"));
 
+// Genero un numero casuale per il computer
+function generaNumeroCasuale() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+let numeroComputer = generaNumeroCasuale();
+
+// Calcolo la somma dei numeri
+let somma = numeroUtente + numeroComputer;
+
